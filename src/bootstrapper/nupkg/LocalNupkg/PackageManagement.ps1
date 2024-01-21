4d05faa2d4378efe92933cd37be3450fc68765ed
Blue Falcon <anon@hostpi.onmicrosoft.com>
2024-01-21 10:55:40 -0700

    feat: :sparkles: add SelectBest() method for picking the correct package from all caches

--- src/bootstrapper/nupkg/LocalNupkg/PackageManagement.ps1
+++ src/bootstrapper/nupkg/LocalNupkg/PackageManagement.ps1
@@ -130,6 +130,67 @@ param(
                     $this.GetFromCache( $Name, $PatchPath )
                 }
 
+            $sources | Add-Member `
+                -MemberType ScriptMethod `
+                -Name SelectBest `
+                -Value {
+                    param(
+                        [string] $Name,
+                        [string] $Version
+                    )
+
+                    If( [string]::IsNullOrWhiteSpace( $Name ) ){
+                        Throw "Name cannot be null or whitespace"
+                    }
+
+                    $main = $this.GetFromMain( $Name )
+                    $cache = $this.GetFromCache( $Name )
+                    $patch = $this.GetFromPatches( $Name )
+
+                    If( [string]::IsNullOrWhiteSpace( $Version ) ){
+                        $combined = New-Object System.Collections.ArrayList
+                        If( $main.Count -eq 1 ){
+                            $combined.Add( $main.Version )
+                        } Elseif( $main.Count ) {
+                            $combined.AddRange( $main.Version )
+                        }
+                        If( $cache.Count -eq 1 ){
+                            $combined.Add( $cache.Version )
+                        } Elseif( $cache.Count ) {
+                            $combined.AddRange( $cache.Version )
+                        }
+                        If( $patch.Count -eq 1 ){
+                            $combined.Add( $patch.Version )
+                        } Elseif( $patch.Count ) {
+                            $combined.AddRange( $patch.Version )
+                        }
+
+                        [Array]::Sort[string]( $combined, [System.Comparison[string]]({
+                            param($x, $y)
+                            $x = $Bootstrapper.SemVer.Parse( $x )
+                            $y = $Bootstrapper.SemVer.Parse( $y )
+            
+                            $Bootstrapper.SemVer.Compare( $x, $y )
+                        }))
+
+                        $combined = $combined | Sort-Object -Unique
+                        
+                        $Version = $combined | Where-Object {
+                            $parsed = $Bootstrapper.Semver.Parse( $_ )
+            
+                            -not( $parsed.PreRelease <# -or $parsed.LegacyPrerelease #> )
+                        } | Select-Object -Last 1
+                    }
+
+                    $package = @(
+                        ($main | Where-Object { $_.Version -eq $Version }), 
+                        ($cache | Where-Object { $_.Version -eq $Version }),
+                        ($patch | Where-Object { $_.Version -eq $Version })
+                    ) | Where-Object { $_ } | Select-Object -First 1
+
+                    $package
+                }
+
             $sources
         })
 }
