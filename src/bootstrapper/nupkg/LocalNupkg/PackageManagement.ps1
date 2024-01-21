da39957bce7f6350c0295dffec8575bf153a158f
Blue Falcon <anon@hostpi.onmicrosoft.com>
2024-01-21 10:52:02 -0700

    perf: :zap: improve GetFromMain() performance by skipping PackageManagement

--- src/bootstrapper/nupkg/LocalNupkg/PackageManagement.ps1
+++ src/bootstrapper/nupkg/LocalNupkg/PackageManagement.ps1
@@ -18,8 +18,33 @@ param(
                 -MemberType ScriptMethod `
                 -Name GetFromMain `
                 -Value {
-                    param( $Name )
-                    Get-Package $Name -AllVersions -ProviderName NuGet -ErrorAction SilentlyContinue
+                    param(
+                        [string] $Name
+                    )
+
+                    If( [string]::IsNullOrWhiteSpace( $Name ) ){
+                        Throw "Name cannot be null or whitespace"
+                    }
+
+                    # see: https://github.com/OneGet/oneget/blob/WIP/src/Microsoft.PackageManagement/Implementation/PackageManagementService.cs#L149-L195
+                    $basepaths = @{}
+
+                    If( [Environment]::SpecialFolder ){
+                        $basepaths.User = [Environment]::GetFolderPath( [Environment+SpecialFolder]::LocalApplicationData )
+                        $basepaths.System = [Environment]::GetFolderPath( [Environment+SpecialFolder]::ProgramFiles )
+                    } Else {
+                        $basepaths.User = [Environment]::GetFolderPath( "LocalApplicationData" )
+                        $basepaths.System = [Environment]::GetFolderPath( "ProgramFiles" )
+                    }
+
+                    $cache_paths = @(
+                        Join-Path $basepaths.User "PackageManagement\NuGet\Packages"
+                        Join-Path $basepaths.System "PackageManagement\NuGet\Packages"
+                    )
+
+                    $this.GetFromCache( $Name, $cache_paths )
+
+                    # Get-Package $Name -AllVersions -ProviderName NuGet -ErrorAction SilentlyContinue
                 }
 
             $sources | Add-Member `
