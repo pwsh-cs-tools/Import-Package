28550db1e6588af8eab31f13e43784f2aed8fde9
Blue Falcon <anon@hostpi.onmicrosoft.com>
2024-01-20 23:15:00 -0700

    fix: :sparkles: fix long-time broken NuGet range handling

--- src/bootstrapper/data/SemVer.ps1
+++ src/bootstrapper/data/SemVer.ps1
@@ -98,6 +98,150 @@ param(
             return 0
         }
 
+    # Parse a NuGet-spec version range
+    # - see https://learn.microsoft.com/en-us/nuget/concepts/package-versioning?tabs=semver20sort
+    $semantic_versioning_reader | Add-Member `
+        -MemberType ScriptMethod `
+        -Name ParseRange `
+        -Value {
+            param(
+                $Range,
+                $Name
+            )
+            $Range = $Range.Trim()
+            
+            $parsed = @{
+                MinVersion = $null
+                MaxVersion = $null
+                MinVersionInclusive = $null
+                MaxVersionInclusive = $null
+            }
+
+            If( [string]::IsNullOrWhiteSpace( $Range ) ){
+                Write-Warning "[Import-Package:Internals] Blank range is not a valid version range. Assuming [0.0.0, )]"
+                $parsed.MinVersion = "0.0.0"
+                $parsed.MinVersionInclusive = $true
+                $parsed.MaxVersion = $null
+                $parsed.MaxVersionInclusive = $false
+            } Else {
+                $versions = $Range.Split( ',' )
+                if( $versions.Count -eq 1 ){
+                    if( $versions -match "[\[\(]" ){
+                        $parsed.MinVersion = $versions[0].TrimStart( '[', '(' ).TrimEnd( ']', ')' )
+                        $parsed.MinVersionInclusive = $true
+                        $parsed.MaxVersion = $parsed.MinVersion
+                        $parsed.MaxVersionInclusive = $true
+                        If( $versions[0].StartsWith("(") -or $versions[0].EndsWith(")") ) {
+                            If( $parsed.MinVersion ){
+                                Write-Warning "[Import-Package:Internals] $Range is not a valid version range. Assuming exact version: [$( $parsed.MinVersion )]"
+                            }
+                        }
+                    } else {
+                        $parsed.MinVersion = $versions[0]
+                        $parsed.MinVersionInclusive = $true
+                        # $parsed.MaxVersion is unbounded
+                    }
+                } else {
+                    if( $versions[0] -and ($versions[0] -match "[\[\(]") ){
+                        $parsed.MinVersion = $versions[0].TrimStart( '[', '(' )
+                        $parsed.MinVersionInclusive = $versions[0].StartsWith( '[' )
+                    } else {
+                        $parsed.MinVersion = $versions[0]
+                        $parsed.MinVersionInclusive = $true
+                    }
+                    if( $versions[1] -and ($versions[1] -match "[\]\)]") ){
+                        $parsed.MaxVersion = $versions[1].TrimEnd( ']', ')' )
+                        $parsed.MaxVersionInclusive = $versions[1].EndsWith( ']' )
+                    } else {
+                        $parsed.MaxVersion = $versions[1]
+                        $parsed.MaxVersionInclusive = $true
+                    }
+                }
+    
+                If( -not $parsed.MinVersion -and -not $parsed.MaxVersion ){
+                    Write-Warning "[Import-Package:Internals] $Range is not a valid version range. Assuming [0.0.0, )]"
+                    $parsed.MinVersion = "0.0.0"
+                    $parsed.MinVersionInclusive = $true
+                    $parsed.MaxVersion = $null
+                    $parsed.MaxVersionInclusive = $false
+                }
+    
+                If( $parsed.MaxVersionInclusive -and -not $parsed.MaxVersion ){
+                    Write-Warning "[Import-Package:Internals] $Range is not a valid version range. Assuming unbounded maximum version."
+                    $parsed.MaxVersionInclusive = $false
+                }
+    
+                If( $parsed.MinVersionInclusive -and -not $parsed.MinVersion ){
+                    Write-Warning "[Import-Package:Internals] $Range is not a valid version range. Assuming unbounded minimum version."
+                    $parsed.MinVersionInclusive = $false
+                }
+            }
+
+            If( $parsed.MinVersion ){
+                $parsed.MinVersion = $Bootstrapper.SemVer.Parse( $parsed.MinVersion )
+            }
+            If( $parsed.MaxVersion ){
+                $parsed.MaxVersion = $Bootstrapper.SemVer.Parse( $parsed.MaxVersion )
+            }
+
+            If( $Name ){
+
+                $all_versions = $Bootstrapper.VersionEndpoints.GetAllVersions( $Name )
+                $all_versions = $all_versions | ForEach-Object {
+                    $Bootstrapper.SemVer.Parse( $_ )
+                }
+    
+                $effective_range = @{
+                    MinVersion = $null
+                    MaxVersion = $null
+                    MinVersionInclusive = $true
+                    MaxVersionInclusive = $true
+                }
+    
+                If( -not $parsed.MaxVersion ){
+                    $effective_range.MaxVersion = $all_versions | Where-Object {
+                        -not $_.PreRelease -and -not $_.LegacyPrerelease
+                    } | Select-Object -Last 1
+                } Else {
+                    $effective_range.MaxVersion = & {
+                        $acceptable_versions = $all_versions | Where-Object {
+                            If( $parsed.MaxVersionInclusive ){
+                                $Bootstrapper.SemVer.Compare( $_, $parsed.MaxVersion ) -le 0
+                            } Else {
+                                $Bootstrapper.SemVer.Compare( $_, $parsed.MaxVersion ) -lt 0
+                            }
+                        } | Where-Object {
+                            -not $_.PreRelease -and -not $_.LegacyPrerelease
+                        }
+                        $acceptable_versions | Select-Object -Last 1
+                    }
+                }
+    
+                If( -not $parsed.MinVersion ){
+                    $effective_range.MinVersion = $all_versions | Where-Object {
+                        -not $_.PreRelease -and -not $_.LegacyPrerelease
+                    } | Select-Object -First 1
+                } Else {
+                    $effective_range.MinVersion = & {
+                        $acceptable_versions = $all_versions | Where-Object {
+                            If( $parsed.MinVersionInclusive ){
+                                $Bootstrapper.SemVer.Compare( $_, $parsed.MinVersion ) -ge 0
+                            } Else {
+                                $Bootstrapper.SemVer.Compare( $_, $parsed.MinVersion ) -gt 0
+                            }
+                        } | Where-Object {
+                            -not $_.PreRelease -and -not $_.LegacyPrerelease
+                        }
+                        $acceptable_versions | Select-Object -First 1
+                    }
+                }
+
+                $effective_range
+            } Else {
+                $parsed
+            }
+        }
+
     $Bootstrapper | Add-Member `
         -MemberType NoteProperty `
         -Name SemVer `
